04f041094f2d7bdec6bbb18bf6a4456c755c4d9e
robstarbuck <robstarbuck@gmail.com>
2022-05-31 16:47:03 +0100

    Global style added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,58 @@
+import { createGlobalStyle } from "styled-components";
+import { BlogCollection } from "../components/BlogCollection";
+
+const GlobalStyle = createGlobalStyle`
+  /* Colours */
+  :root {
+    --heading: #5045CD;
+    --accent: #A8B6FF;
+    --background: #F5F8FA;
+    --foreground: #FFFFFF;
+  }
+
+  /* Vertical Rhythm - tile.css */
+  :root {
+    --tile: 20px;
+  }
+
+  * {
+    font-size: calc(
+        0.655 *
+        var(--tile) *
+        var(--tile-count, 1)
+    );
+    line-height: calc(
+        var(--tile) *
+        var(--tile-count, 1) *
+        var(--tile-factor, 1)
+    );
+  }
+
+  h1 {
+      --tile-count: 3;
+      --tile-factor: 0.75;
+  }
+
+  h2 {
+      --tile-count: 2;
+      --tile-factor: 0.75;
+  }
+
+  h3 {
+      --tile-count: 1;
+  }
+
+  /* Body */
+
+  body {
+    background: var(--background);
+    font-family: Arial, sans-serif;
+  }
+`
+
 export default function BlogListingPage() {
-  return <div>...</div>;
+  return <div>
+    <GlobalStyle />
+    <BlogCollection />
+  </div>;
 }
