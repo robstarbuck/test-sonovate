b231dded48d1b84d3b169368f58113bfa24f3486
robstarbuck <robstarbuck@gmail.com>
2022-05-31 17:56:14 +0100

    Blog Post styling and type correction

--- src/components/BlogPost/index.tsx
+++ src/components/BlogPost/index.tsx
@@ -1,29 +1,42 @@
 import Link from "next/link";
-import { ReactElement } from "react";
+import { ReactNode } from "react";
 import styled, { CSSProperties } from "styled-components";
 
 type BlogPostCardProps = {
   loading?: boolean;
   style?: CSSProperties;
   linkTo?: string;
-  children?: ReactElement;
+  children?: ReactNode;
 };
 
 const Container = styled.div<BlogPostCardProps>`
-  background: ${(props) =>
-    props.loading ? "var(--background)" : "var(--foreground)"};
+    padding: var(--tile);
+    overflow: hidden;
+    position: relative;
+    background: ${(props) =>
+        props.loading ? "red" : "var(--foreground)"};
 `;
+
+const LinkFooter = styled.footer`
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    flex-direction: row-reverse;
+    padding: var(--tile);
+`
 
 export const BlogPostCard = (props: BlogPostCardProps) => {
   const { style, children, linkTo, loading } = props;
   return (
-    <Container style={style}>
+    <Container style={style} loading={loading}>
       <>
         {children}
         {linkTo && (
-          <footer>
+          <LinkFooter>
             <Link href={linkTo}>TODO - ARROW RIGHT</Link>
-          </footer>
+          </LinkFooter>
         )}
       </>
     </Container>
