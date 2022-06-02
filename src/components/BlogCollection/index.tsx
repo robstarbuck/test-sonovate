f93e3315d48be0c507108d9573989116e9772fa2
robstarbuck <robstarbuck@gmail.com>
2022-06-02 16:02:50 +0100

    Lifted BlogCollection props

--- src/components/BlogCollection/index.tsx
+++ src/components/BlogCollection/index.tsx
@@ -1,26 +1,25 @@
-import { useQuery } from "@apollo/client";
-import gql from "graphql-tag";
+import { ApolloError } from "@apollo/client";
 import styled from "styled-components";
 import { BlogPostItem } from "../../pages/[id]";
 import { CardLink } from "../Card";
 import PageError from "../PageError";
 import { Title } from "../Title";
 
-type BlogPostCollection = {
-  blogPostCollection: {
-    items: ReadonlyArray<BlogPostItem>;
-  };
-};
+
 
 // https://graphql.contentful.com/content/v1/spaces/jgxvzzx7ps77/explore?access_token=AEf7QMYxPL9rGzq0iYw8vNWzbRvGEhLrtPXHYWYYE_I
 
-export const BlogCollection = () => {
-  const { loading, data, error } = useQuery<BlogPostCollection>(
-    BLOG_POST_COLLECTION_QUERY, {fetchPolicy: "no-cache"}
-  );
+type BlogCollectionProps = {
+  items?: ReadonlyArray<BlogPostItem>;
+  loading?: boolean;
+  error?: ApolloError;
+}
+
+export const BlogCollection = (props: BlogCollectionProps) => {
+  const {items, error, loading} = props;
 
   const skeleton = Array<undefined>(10).fill(undefined);
-  const itemsOrSkeleton = loading ? skeleton : data?.blogPostCollection.items;
+  const itemsOrSkeleton = loading ? skeleton : items;
 
   return (
     <Container>
@@ -52,20 +51,7 @@ export const BlogCollection = () => {
   );
 };
 
-const BLOG_POST_COLLECTION_QUERY = gql`
-  query {
-    blogPostCollection(limit: 10) {
-      items {
-        sys {
-          id
-        }
-        title
-        preface
-        body
-      }
-    }
-  }
-`;
+
 
 const Container = styled.div`
   --slim: 33.33%;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,46 @@
+import { gql, useQuery } from "@apollo/client";
 import { BlogCollection } from "../components/BlogCollection";
 import { CopyRight } from "../components/CopyRight";
 import { Layout } from "../components/Layout";
 import { SectionTitle } from "../components/SectionTitle";
+import { BlogPostItem } from "./[id]";
+
+type BlogPostCollection = {
+  blogPostCollection: {
+    items: ReadonlyArray<BlogPostItem>;
+  };
+};
 
 export default function BlogListingPage() {
+  const { loading, data, error } = useQuery<BlogPostCollection>(
+    BLOG_POST_COLLECTION_QUERY
+  );
   return (
     <Layout.Trunk>
-      <SectionTitle>
-        From the Blog
-      </SectionTitle>
+      <SectionTitle>From the Blog</SectionTitle>
       <Layout.Content>
-        <BlogCollection />
+        <BlogCollection
+          loading={loading}
+          items={data?.blogPostCollection.items}
+          error={error}
+        />
       </Layout.Content>
       <CopyRight />
     </Layout.Trunk>
   );
 }
 
-
-
+const BLOG_POST_COLLECTION_QUERY = gql`
+  query {
+    blogPostCollection(limit: 10) {
+      items {
+        sys {
+          id
+        }
+        title
+        preface
+        body
+      }
+    }
+  }
+`;
